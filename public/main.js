2a68f98725073d1509d9813d2a42757098ec2ea9
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-11-14 22:04:53 -0500

    finished fizzbuzz

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
-const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('fizzbuzz')
+  } else if (i % 5 === 0) {
+    console.log('buzz')
+  } else if (i % 3 === 0) {
+    console.log('fizz')
+  } else {
+    console.log(i)
+  }
 }
-
-document.addEventListener('DOMContentLoaded', main)
-
